7339b71b43acdf9cb594b144eeea84d059899fdd
Austin Poulson <austinpoulson@gmail.com>
2023-07-07 17:28:44 -0500

    Add links

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+
+import Twitter from './assets/twitter.svg';
+import Reddit from './assets/reddit.svg';
+import YouTube from './assets/youtube.svg';
+import Twitch from './assets/twitch.svg';
+import Instagram from './assets/instagram.png';
+import Netflix from './assets/netflix.svg';
+import Amazon from './assets/amazon.svg';
+import SoundCloud from './assets/soundcloud.svg';
+import Gmail from './assets/gmail.svg';
+import StackOverflow from './assets/stackoverflow.svg';
+import Github from './assets/github.svg';
+import HF from './assets/hacker.png';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='LinkList'>
+        <Link name='Twitter' icon={Twitter} link='https://twitter.com/'/>
+        <Link name='Reddit' icon={Reddit} link='https://www.reddit.com/'/>
+        <Link name='YouTube' icon={YouTube} link='https://www.youtube.com/'/>
+        <Link name='Twitch' icon={Twitch} link='https://www.twitch.tv/'/>
+        <Link name='Instagram' icon={Instagram} link='https://www.instagram.com/'/>
+        <Link name='Netflix' icon={Netflix} link='https://www.netflix.com/'/>
+        <Link name='Amazon' icon={Amazon} link='https://www.amazon.com/'/>
+        <Link name='SoundCloud' icon={SoundCloud} link='https://soundcloud.com/'/>
+        <Link name='Gmail' icon={Gmail} link='https://mail.google.com/'/>
+        <Link name='Stack Overflow' icon={StackOverflow} link='https://stackoverflow.com/'/>
+        <Link name='HF' icon={HF} link='https://hackforums.net/'/>
+        <Link name='Github' icon={Github} link='https://github.com/'/>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Link({name, icon, link}) {
+  return (
+    <a className='LinkContainer' href={link}>
+      <div className='LinkIconContainer'>
+        <img className='LinkImage' src={icon} alt={name}></img>
+      </div>
+      <div className='LinkTextContainer'>
+        <p className='LinkText'>{name}</p>
+      </div>
+      <div className='LinkStatusContainer'></div>
+    </a>
+  )
+}
+
